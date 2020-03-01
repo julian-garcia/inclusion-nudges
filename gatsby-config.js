ea9e162f96145c639d32e8bc223b9725c022fe74
Julian Garcia <julian.garcia@directlinegroup.co.uk>
2020-03-01 18:11:15 +0000

    Initial setup for CMS markdown processing

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,17 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `nudges`,
+        path: `${__dirname}/nudges`,
+      },
+    },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
