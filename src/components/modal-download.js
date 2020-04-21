b100c94b084f67db9fbe263f53a0f6521b50e8d3
Julian Garcia <julian.garcia.leoni@gmail.com>
2020-04-21 18:35:13 +0100

    Download modal changes and events CMS config

--- src/components/modal-download.js
+++ src/components/modal-download.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Modal, { showModal } from "../components/modal"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import { faTwitter, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
 import addToMailchimp from "gatsby-plugin-mailchimp"
 import GuidebookPDF from "../docs/Guidebook_Quick-Guide.pdf"
@@ -51,10 +52,16 @@ const ModalDownload = () => {
       </form>
     </Modal>
     <Modal modalClass="modal-download-confirm">
-      <h3 className="modal-title">Thank you, please pay with a Tweet or a LinkedIn share</h3>
+      <h3 className="modal-title">Enjoy your Quick Guide.<br/>Please tell people in your network about Inclusion&nbsp;Nudges</h3>
       <div style={{textAlign:'center'}} className="modal-social-icons">
+        <p style={{margin:'1rem 0 0'}}>We premade a <strong>Twitter</strong> text – you can use that or make your own.</p>
         <FontAwesomeIcon icon={faTwitter} className="modal-social-icon apply-link-style" onClick={(e) => socialShare('https://twitter.com/intent/tweet?url=https%3A%2F%2Finclusion-nudges.org&text=Check%20out%20%23InclusionNudges%20Guidebook.%20You%20get%20100%20examples%20of%20behavioural%20%23designs%20to%20%23debias%20%26%20make%20%23inclusion%20%26%20%23diversity%20the%20norm%20in%20your%20organisation%2C%20community%20and%20society.%20%20A%20%23HowTo%20guide%20for%20%23ChangeMakers%21', 'Tweet')} />
-        <FontAwesomeIcon icon={faLinkedinIn} className="modal-social-icon apply-link-style" onClick={(e) => socialShare('https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Finclusion-nudges.org%2F', 'LinkedIn share')} />
+        <p style={{margin:'1rem 0 0'}}>We also have an <strong>e-mail</strong> suggestion for you.</p>
+        <a href="mailto:?subject=Inclusion%20Nudges&body=Hi!%20I%20just%20visited%20this%20website%20and%20thought%20you%20might%20find%20the%20Inclusion%20Nudges%20global%20initiative%20and%20the%20Inclusion%20Nudges%20Guidebook%20interesting.%20%0Awww.inclusion-nudges.org" target="_blank" rel="noopener noreferrer" className="modal-social-link">
+          <FontAwesomeIcon icon={faEnvelope} className="modal-social-icon" />
+        </a>
+        <p style={{margin:'1rem 0 0'}}>For a post on LinkedIn,  here is a suggestion:<br/>I found this interesting –  you might find the Inclusion Nudges Hub and Guidebook inspiring as well. Check out www.inclusion-nudges.org</p>
+        <FontAwesomeIcon icon={faLinkedinIn} className="modal-social-icon apply-link-style" onClick={(e) => socialShare('https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Finclusion-nudges.org%2F&summary=Test', 'LinkedIn share')} style={{marginBottom:'2rem'}} />
       </div>
     </Modal>
   </>
