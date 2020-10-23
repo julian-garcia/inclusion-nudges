4d012bba91776bea47ae9aa33932633771871b57
Julian Garcia <julian.garcia.leoni@gmail.com>
2020-10-23 18:21:30 +0100

    Move blog link
    Move the blog link to the first position on the home page to improve exposure on mobile devices

--- src/components/headline.js
+++ src/components/headline.js
@@ -9,6 +9,16 @@ const Headline = () => (
   <>
     <h2 className="page-heading">Inclusion Nudges change approach and global initiative </h2>
     <div className="row-auto approach">
+      <div className="accent-3 grid-item-image" style={{textAlign:'center'}}>
+        <Link to="/blog" className="unstyled-button"></Link>
+        <div style={{position:'static', padding:'0'}}>
+          <h3>Read | Listen | Watch</h3>
+          <h2><span style={{fontWeight:'normal'}}>The<br/>Inclusion&nbsp;Nudges</span><br/>BLOG</h2>
+          <h3 className="modal-form__button subscribe ">
+            Subscribe
+          </h3>
+        </div>
+      </div>
       <div className="grid-item-image" style={{backgroundImage:`url(${WhyImg})`}}>
         <Link to="/blog/about-inclusion-nudges/power-of-inclusion-nudges" className="unstyled-button"></Link>
         <div>
@@ -31,16 +41,6 @@ const Headline = () => (
           <h2>How</h2>
           <h3 className="headline">Empowering you to apply Inclusion Nudges to engage all people in making systems, cultures, and behaviours inclusive as the norm</h3>
           <h3 className="apply-link-style approach-learn-more">Learn more...</h3>
-        </div>
-      </div>
-      <div className="accent-3 grid-item-image" style={{textAlign:'center', alignItems:'center'}}>
-        <Link to="/blog" className="unstyled-button"></Link>
-        <div style={{position:'static', padding:'0'}}>
-          <h3>Read | Listen | Watch</h3>
-          <h2><span style={{fontWeight:'normal'}}>The<br/>Inclusion&nbsp;Nudges</span><br/>BLOG</h2>
-          <h3 className="modal-form__button subscribe ">
-            Subscribe
-          </h3>
         </div>
       </div>
       <Link to="/blog/about-inclusion-nudges/power-of-inclusion-nudges" className="approach-link">Learn more ABOUT Inclusion&nbsp;Nudges HERE</Link>
